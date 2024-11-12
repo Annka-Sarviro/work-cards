b0b5a128a13b29dac5ce1b0a30e2171ba509caf4
Annka-Sarviro <annka_nuha@i.ua>
2024-11-12 18:49:41 +0200

    feat:activity oage

--- app/(platform)/(dashboard)/organization/[organizationId]/activity/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/activity/page.tsx
@@ -1,7 +1,18 @@
-import { OrganizationProfile } from '@clerk/nextjs';
+import { Separator } from '@/components/ui/separator';
+import { Info } from '../_components/info';
+import { ActivityList } from './_components/activity-list';
+import { Suspense } from 'react';
 
-const ActivityPage = () => {
-    return <div className="w-full">ACtPAeg</div>;
+const ActivityPage = async () => {
+    return (
+        <div className="w-full">
+            <Info />
+            <Separator className="my-2" />
+            <Suspense fallback={<ActivityList.Skeleton />}>
+                <ActivityList />
+            </Suspense>
+        </div>
+    );
 };
 
 export default ActivityPage;
